9fa0cfff372f5c060c342d01cb28cf5b3827d5f9
Duncan Donaldson <donaldsonduncan@ymail.com>
2011-03-18 13:59:20 -0700

    some random changes that I don't remember what they do or if they compile

--- src/network/network.h
+++ src/network/network.h
@@ -8,6 +8,6 @@
 #define PACKETSIZE 4096
 void WinsockInit();
 void WinsockCleanup();
-void mkPacket(char* buf, unsigned char msgtype, unsigned char srcClient,
-              unsigned char destClient, char* data);
+void mkPacket(char* buf, char msgtype, char srcClient,
+              char destClient, char* data);
 #endif
