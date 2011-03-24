a702416652c0ebdeef7812334b21c3b4805ca525
unknown <Admin@.(none)>
2011-03-24 10:13:17 -0700

    TCP send test

--- src/control/connectioncontrol.cpp
+++ src/control/connectioncontrol.cpp
@@ -15,8 +15,6 @@ bool ConnectionControl::startServer(int tcpPort, int udpPort) {
 }
 
 bool ConnectionControl::connectToServer(QString tcpIp, int tcpPort, int udpPort) {
-    //socket_.TCPSocket_Connect(ip.toAscii().data(), port); //<< if this fail return false to notify the gui
-    memcpy(socket_.packet_, "test\0", 5);
     socket_.UDPSocket_Init();
     socket_.createOLEvent();
     socket_.UDPSend_Multicast();
