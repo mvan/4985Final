79f00c1987129d92b6af16e86deff377d76787d9
ZpHiNGPhOnG~ <pkhantidhara@zinzone.net>
2011-04-04 04:25:36 +0700

    Uncomment server comtrol

--- src/control/servercontrol.cpp
+++ src/control/servercontrol.cpp
@@ -7,10 +7,10 @@ ServerControl::~ServerControl() {
 }
 
 bool ServerControl::addAudioFile(QString filename) {
-    //clientsSocket_ = connectionControl_->getTCPServer()->getAllClients();
-    //foreach(sock socket, clientsSocket_) {
-      //  mkPacket(socket.packet_, MSG_LIST, filename.size(), 0x00, filename.toAscii().data()); //Change the client destination
-        //socket.TCPSend();
-    //}
+    clientsSocket_ = connectionControl_->getTCPServer()->getAllClients();
+    foreach(sock socket, clientsSocket_) {
+        mkPacket(socket.packet_, MSG_LIST, filename.size(), 0x00, filename.toAscii().data()); //Change the client destination
+        socket.TCPSend();
+    }
     return true;
 }
