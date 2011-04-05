ce0c50c37b2bacbeedc7a6d10cc299d53a5a9f25
unknown <Admin@.(none)>
2011-04-05 14:04:48 -0700

    micthread

--- src/audio/micthread.cpp
+++ src/audio/micthread.cpp
@@ -1,16 +1,26 @@
 #include "micthread.h"
 #include "audioin.h"
+#include "../network/audiotransfer.h"
 bool micOut = FALSE;
 MicThread::MicThread(){}
 
 void MicThread::run(){
 
+    AudioSendThread *thread;
     audioin in;
+
+    //connect(thread, SIGNAL(sendPacket(char*)), this, SLOT(send(char*)), Qt::QueuedConnection);
     in.setupParams();
     in.createAudioDev();
+
+    thread->start();
 
     while(micOut){
         in.readSound();
     }
 
 }
+
+/*void MicThread::send(char*){
+
+}*/
