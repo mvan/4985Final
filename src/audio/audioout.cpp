12633a6aab38b8882dae9a3aca81558f967cba4b
unknown <Admin@.(none)>
2011-04-06 05:03:41 -0700

    merged stash pop

--- src/audio/audioout.cpp
+++ src/audio/audioout.cpp
@@ -13,10 +13,12 @@ void audioout::setupParams() {
 int audioout::getParams(const char* params) {
     if(memcmp(params, hdr_, HDR_SIZE) != 0) {
         memmove(hdr_, params, HDR_SIZE);
+
         memmove(&frequency_, hdr_+24, 4);
         memmove(&byteRate_, hdr_+28, 4);
         memmove(&channels_, hdr_+22, 2);
         memmove(&sampleSize_, hdr_+34, 2);
+
         if((byteRate_) != 0) {
             pause_ = ((double)1/(((double)byteRate_)/((double)AUDIO_DATA_SIZE)));
         }
