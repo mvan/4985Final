fd10047aa855625b5ebd86d3b64ddd8ab551825d
Duncan Donaldson <donaldsonduncan@ymail.com>
2011-04-06 07:57:38 -0700

    some comments

--- src/network/socket.h
+++ src/network/socket.h
@@ -36,40 +36,149 @@ class sock {
         virtual ~sock() {
             delete mut_;
         }
-
+        /**
+          * TCPSocket_Init()
+          *
+          * Description:
+          * This function initializes a socket as a TCP socket.
+          */
         void TCPSocket_Init();
+        /**
+          * TCPSocket_bind()
+          *
+          * @param portNo - the port to bind the socket to.
+          *
+          * Description:
+          * This function binds a TCP socket to a specified port.
+          */
         void TCPSocket_Bind(int portNo);
+        /**
+          * TCPSocket_Listen()
+          *
+          *
+          * Description:
+          * This function sets a TCP socket to the listen state.
+          */
         void TCPSocket_Listen();
+        /**
+          * TCPSocket_Connect(char* servAddr, int portNo)
+          *
+          * @param char* servAddr - the server address to connect to.
+          * @param porno* the port to connect over.
+          *
+          * @return BOOL - returns true on succes, true on failure
+          *
+          * Description:
+          * This function sets a TCP socket to the listen state.
+          */
         BOOL TCPSocket_Connect(char* servAddr, int portNo);
+        /**
+          * TCPSocket_Accept()
+          *
+          *
+          * @return SOCKET - returns the newly accepted socket.
+          *
+          * Description:
+          * This accepts a connection request from a listening a TCP socket.
+          */
         SOCKET TCPSocket_Accept();
-
+        /**
+          * UDPSocket_Init()
+          *
+          *
+          * Description:
+          * Initializes a socket to be a UDP socket
+          */
         void UDPSocket_Init(int portNo);
+        /**
+          * UDPSocket_Bind_Multicast()
+          *
+          * @return BOOL returns true on success, false if anything fails.
+          *
+          * Description:
+          * binds a UDP socket to a port, and sets it up for multicasting.
+          */
         BOOL UDPSocket_Bind_Multicast();
-
+        /**
+          * TCPSend()
+          *
+          * @return int returns the number of bytes sent.
+          *
+          * Description:
+          * Sends a packet over a TCP socket..
+          */
         int TCPSend();
+        /**
+          * UDPSend_Multicast()
+          *
+          * @return int returns the number of bytes sent.
+          *
+          * Description:
+          * Multicasts a packet over a UDP socket.
+          */
         int UDPSend_Multicast();
+        /** TCPRecv()
+        *
+        * @return int returns the number of bytes read.
+        *
+        * Description:
+        * Reads a packet off of a TCP socket.  */
         int TCPRecv();
+        /** UDPRecv_Multicast()
+        *
+        * @return int returns the number of bytes read.
+        *
+        * Description:
+        * Reads a packet off of a UDP multicast socket.  */
         int UDPRecv_Multicast();
-
+        /** setLocalAddr()
+        *
+        *
+        * Description:
+        * Fills the localaddr_ member of the socket class with the
+        * IP address of the computer the program is currently running on */
         void setLocalAddr() {
             struct sockaddr_in s;
             socklen_t size;
             getsockname(sock_, (struct sockaddr*)&s, &size);
             strcpy(localaddr_, inet_ntoa(s.sin_addr));
         }
+        /** getLocalAddr()
+        *
+        *
+        * @return the computer's local address
+        *
+        * Description:
+        * getter for localaddr_ */
         char* getLocalAddr() {
             return localaddr_;
         }
-
+        /** clrPacket()
+        *
+        *
+        * Description:
+        * clears the current socket's packet_ array.*/
         //inline functions
         void clrPacket() {
             mut_->lock();
             ZeroMemory(packet_, PACKETSIZE);
             mut_->unlock();
         }
+        /** getSock()
+        *
+        * @return SOCKET the socket held in the current sock object
+        *
+        * Description:
+        * getter for getSock*/
         SOCKET getSock() {
             return sock_;
         }
+        /** setSock()
+        *
+        * @param sock the socket held in the current sock object
+        *
+        * Description:
+        * getter for getSock*/
         void setSock(SOCKET sock) {
             sock_ = sock;
         }
--- src/network/tcpserver.h
+++ src/network/tcpserver.h
@@ -28,10 +28,51 @@ class tcpserver: public server {
             listenSock_->socket_close();
             delete listenSock_;
         }
+        /**
+          * initSelect()
+          *
+          *
+          * Description:
+          * This function initiazlizes the tcpserver for select.
+          */
         virtual void initSelect();
+        /**
+          * addSelectSock()
+          *
+          * @return returns the newly accepted and added socket.
+          *
+          * Description:
+          * accepts an incoming connection request and adds the socket
+          * to the select sets.
+          */
         SOCKET addSelectSock();
+        /**
+          * removeSelectSock(SOCKET s)
+          *
+          * @param the socket to be removed from the sets.
+          *
+          * Description:
+          * removes a socket from the select sets.
+          */
         void removeSelectSock(SOCKET s);
+        /**
+          * run()
+          *
+          *
+          * Description:
+          * Runs the TCP server.
+          */
         virtual void run();
+        /**
+          * ProcessTCPPacket(char* packet)
+          *
+          * @param the packet to be processed.
+          *
+          * @return -1 if the packet is a connection notification, 1 otherwise.
+          *
+          * Description:
+          * Handles a received TCP packet.
+          */
         int ProcessTCPPacket(char* packet);
 
     signals:
