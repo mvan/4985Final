69b7b9d0b4d4321bb31bca2875918ac97493ad1b
Marcel Vangrootheest <mvangrootheest@gmail.com>
2011-04-06 01:58:00 -0700

    audio comments

--- src/audio/audioin.h
+++ src/audio/audioin.h
@@ -1,3 +1,10 @@
+/**
+  * Class: audioin
+  *
+  * Description:
+  * AudioIn is the tool for all audio input. Via file streaming or microphone.
+  */
+
 #ifndef AUDIOIN_H
 #define AUDIOIN_H
 #include <QObject>
@@ -9,23 +16,88 @@
 class audioin: public QObject {
     Q_OBJECT
 
-    private:
-        QAudioInput* input_;
-        QAudioFormat format_;
-        QIODevice* buffer_;
-        QBuffer* inbuf_;
-        qint64 numToRead_;
+private:
+    /** Audio input object that performs operations for input.*/
+    QAudioInput* input_;
 
-        char hdr_[HDR_SIZE];
-    public:
-        explicit audioin():numToRead_(0){}
-        virtual ~audioin(){}
-        void setupParams();
-        void createAudioDev();
-        void destroyAudioDev();
-        void mkVoiceHdr(char* buf);
-    public slots:
-        void readSound();
-        void writeReady(qint64 bytes);
+    /** The format for the current audio input. */
+    QAudioFormat format_;
+
+    /** The buffer connected to AudioInput that all input gets written to. */
+    QIODevice* buffer_;
+
+    /** The buffer used instead of QIODevice for reading input. */
+    QBuffer* inbuf_;
+
+    /** Number of bytes to read from input. */
+    qint64 numToRead_;
+
+    /** The header for the audio. */
+    char hdr_[HDR_SIZE];
+public:
+    explicit audioin():numToRead_(0){}
+    virtual ~audioin(){}
+
+    /**
+      * Function: setupParams()
+      *
+      * @return void
+      *
+      * Description:
+      * Sets up the audio format parameters for a wav file.
+      */
+    void setupParams();
+
+    /**
+      * Function: createAudioDev()
+      *
+      * @return void
+      *
+      * Description:
+      * Sets up the default audio input device and the input buffers.
+      */
+    void createAudioDev();
+
+    /**
+      * Function: destroyAudioDev()
+      *
+      * @return void
+      *
+      * Description:
+      * Cleans up the AudioInput and input buffers.
+      */
+    void destroyAudioDev();
+
+    /**
+      * Function: mkVoiceHdr(char* buf)
+      *
+      * @return void
+      *
+      * @param buf the buffer to append the voice header to.
+      *
+      * Description:
+      * Sets up the audio format header for the microphone.
+      */
+    void mkVoiceHdr(char* buf);
+public slots:
+
+    /**
+      * Slot: readSound()
+      *
+      * Description:
+      * Reads all available data from the audio input device,
+      * and writes it to a buffer.
+      */
+    void readSound();
+
+    /**
+      * Slot: writeReady(qint64 bytes)
+      *
+      * @param bytes Number of bytes available to read
+      *
+      * Description:
+      * Sets numToRead_ to the available bytes to read.
+      */
+    void writeReady(qint64 bytes);
 };
 #endif
--- src/audio/audioout.h
+++ src/audio/audioout.h
@@ -1,3 +1,10 @@
+/**
+  * Class: audioout
+  *
+  * Description:
+  * This class has all the tools for setting up and sending sound
+  * to the audio output device.
+  */
 #ifndef AUDIOOUT_H
 #define AUDIOOUT_H
 #include <QObject>
@@ -7,25 +14,91 @@
 class audioout: public QObject {
     Q_OBJECT
 
-    private:
-        QAudioOutput* output_;
-        QAudioFormat format_;
-        QIODevice* buffer_;
+private:
+    /** AudioOutput object that performs operations for output. */
+    QAudioOutput* output_;
 
-        int frequency_;
-        int byteRate_;
-        double pause_;
-        short channels_;
-        short sampleSize_;
-        char hdr_[HDR_SIZE];
-    public:
-        explicit audioout():output_(NULL), pause_(0){}
-        virtual ~audioout(){}
-        int getParams(const char* params);
-        void setupParams();
-        void createAudioDev();
-        void destroyAudioDev();
-    public slots:
-        void playSound(char* sound);
+    /** The format for the current audio output. */
+    QAudioFormat format_;
+
+    /** The buffer that output_ reads from to play sound. */
+    QIODevice* buffer_;
+
+    /** The frequency of the audio. (For format) */
+    int frequency_;
+
+    /** The byteRate of the audio. (For format) */
+    int byteRate_;
+
+    /** The number of milliseconds for the current audio data. */
+    double pause_;
+
+    /** The number of channels of the audio. (For format) */
+    short channels_;
+
+    /** The sample size of the audio. (For format) */
+    short sampleSize_;
+
+    /** The header for the audio. */
+    char hdr_[HDR_SIZE];
+public:
+    explicit audioout():output_(NULL), pause_(0){}
+    virtual ~audioout(){}
+
+    /**
+      * Function: getParams(const char* params)
+      *
+      * @return 1 if unsuccessful. 0 if successful.
+      *
+      * @param params the data to be parsed for format.
+      *
+      * Description:
+      * Sets up the audio format parameters based on params.
+      */
+    int getParams(const char* params);
+
+    /**
+      * Function: setupParams()
+      *
+      * @return void
+      *
+      * Description:
+      * Sets up the audio format parameters based on the standard wav type.
+      */
+    void setupParams();
+
+    /**
+      * Function: createAudioDev()
+      *
+      * @return void
+      *
+      * Description:
+      * Sets up the default audio output device with the QAudioOutput
+      * using the ouput buffer.
+      */
+    void createAudioDev();
+
+    /**
+      * Function: destroyAudioDev()
+      *
+      * @return void
+      *
+      * Description:
+      * Cleans up the AudioOutput and output buffer.
+      */
+    void destroyAudioDev();
+
+public slots:
+    /**
+      * Function: playSound(char* sound)
+      *
+      * @return void
+      *
+      * @param sound
+      *
+      * Description:
+      * Sends the sound data to the output device.
+      */
+    void playSound(char* sound);
 };
 #endif
